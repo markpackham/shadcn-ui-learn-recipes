7d11ac1478e9bb613974b2d6ee9f78f63a46b1cc
mark <markpackham1@gmail.com>
2024-01-23 11:35:39 +0000

    Add getRecipes function
    https://youtu.be/sXrwh4I229Q?si=jghCWfLOhi3iYREM&t=193

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main>Home</main>;
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const response = await fetch(`http://localhost:4000/recipes`);
+  return response.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map(recipe => ())}
+      </div>
+    </main>
+  );
 }
