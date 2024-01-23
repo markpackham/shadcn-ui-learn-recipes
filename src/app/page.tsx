4356d7cc5856a326f3a4d0f254d579ec2d1f0483
mark <markpackham1@gmail.com>
2024-01-23 12:16:25 +0000

    Use Card component with recipes
    https://youtu.be/sXrwh4I229Q?si=rYMC8Da2I0VU9xFi&t=527

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 interface Recipe {
   title: string;
   image: string;
@@ -18,7 +27,23 @@ export default async function Home() {
   return (
     <main>
       <div className="grid grid-cols-3 gap-8">
-        {recipes.map(recipe => ())}
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="">
+            <CardHeader>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>{recipe.description}</CardDescription>
+            </CardContent>
+            <CardFooter>
+              <button className="m-4">View Recipe</button>
+              <p>{recipe.vegan ? "Vegan" : "Not Vegan!"}</p>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </main>
   );
