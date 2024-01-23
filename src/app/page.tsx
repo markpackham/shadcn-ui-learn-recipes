54a7ae93174487cd842de4fa5f32ba5bd21a87f8
mark <markpackham1@gmail.com>
2024-01-23 13:18:35 +0000

    Import Avatar, Badge & Button
    https://youtu.be/6m8rsHdhOt8?si=Sv8GUrkQ3AAXNxXF&t=169

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 interface Recipe {
   title: string;
