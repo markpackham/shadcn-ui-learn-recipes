66407dbc22444ee1a59a9ef2c41d139bc39e418e
mark <markpackham1@gmail.com>
2024-01-24 13:54:43 +0000

    Put generic Card in SkeletonCard
    https://youtu.be/cCQZ0_y-4Os?si=KJtj0aqSaHUMgP3r&t=354

--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -1,5 +1,15 @@
+import {Card, CardContent, CardHeader, CardFooter} from "./ui/card";
+import { Skeleton } from "./ui/skeleton";
+
 export default function SkeletonCard() {
   return (
-    <div>SkeletonCard</div>
+    <Card className="flex flex-col justify-between">
+    <CardHeader className="flex-row gap-4 items-center">
+    </CardHeader>
+    <CardContent>
+    </CardContent>
+    <CardFooter className="flex justify-between">
+    </CardFooter>
+  </Card>
   )
 }
