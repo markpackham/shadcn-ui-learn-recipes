d44f4eeb35ce7376ce7ff7fa2269d4a7ed47c81e
mark <markpackham1@gmail.com>
2024-01-24 14:08:18 +0000

    Add skeleton card content
    https://youtu.be/cCQZ0_y-4Os?si=Sh59dtjhreQGDgme&t=484

--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -5,10 +5,19 @@ export default function SkeletonCard() {
   return (
     <Card className="flex flex-col justify-between">
     <CardHeader className="flex-row gap-4 items-center">
+      <Skeleton className="w-12 h-12 rounded-full"/>
+      {/* With no width default is used & flex-grow means eat up remaining space */}
+      <Skeleton className="h-6 flex-grow"/>
     </CardHeader>
+    {/* Intended for text */}
     <CardContent>
+     <Skeleton className="h-4 flex-grow mt-4"/>
+     <Skeleton className="h-4 flex-grow mt-4"/>
+     {/* Take up half of the container */}
+     <Skeleton className="h-4 w-1/2 mt-4"/>
     </CardContent>
-    <CardFooter className="flex justify-between">
+    <CardFooter>
+
     </CardFooter>
   </Card>
   )
