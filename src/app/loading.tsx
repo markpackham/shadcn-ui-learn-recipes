941f5da0d016bde3dc3442470eefe494a95dc5f1
mark <markpackham1@gmail.com>
2024-01-24 13:48:26 +0000

    Add SkeletonCard
    https://youtu.be/cCQZ0_y-4Os?si=ggLRl2i4hOfn4CJ3&t=247

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,11 @@
+import SkeletonCard from "@/components/SkeletonCard";
+
 export default function loading() {
   return (
-    <div>loading</div>
+    <main>
+    <div className="grid grid-cols-3 gap-8">
+        {"abcdefghi".split('').map(i => (<SkeletonCard key={i} />))}
+    </div>
+    </main>
   )
 }
